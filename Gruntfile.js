43816b053e3e0d286cb1d97098820ae95b67a8c6
Simon Morley <simon@polkaspots.com>
2018-06-11 11:07:26 +0100

    First commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,13 +39,9 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.client %>/scripts/config.js',
         },
         constants: {
-          // API_END_POINT: 'https://api.ctapp.io/api/v1',
-          // API_URL: 'https://api.ctapp.io',
           API_END_POINT: 'http://mimo.api:3000/api/v1',
           API_URL: 'http://mimo.api:3000',
           ENVIRONMENT: 'development'
-          // API_END_POINT: 'https://api.polkaspots.com/api/v1',
-          // API_URL: 'https://api.polkaspots.com'
         }
       },
       production: {
